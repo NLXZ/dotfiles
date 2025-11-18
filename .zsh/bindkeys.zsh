de2fdc2e7d7905c2e97c637a56e934b1bcc0994b
NLXZ <155401381+NLXZ@users.noreply.github.com>
2025-11-18 15:12:56 +0100

    New config

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -15,25 +15,26 @@ bindkey -M emacs '^[[3;5~' kill-word
 bindkey -M viins '^[[3;5~' kill-word
 bindkey -M vicmd '^[[3;5~' kill-word
 
-# up/down arrow keys (fuzzy find history)
+# up arrow: fuzzy history search
 autoload -U up-line-or-beginning-search
 zle -N up-line-or-beginning-search
 bindkey -M emacs "^[[A" up-line-or-beginning-search
 bindkey -M viins "^[[A" up-line-or-beginning-search
 bindkey -M vicmd "^[[A" up-line-or-beginning-search
-if [[ -n "${terminfo[kcuu1]}" ]]; then
+[[ -n "${terminfo[kcuu1]}" ]] && {
   bindkey -M emacs "${terminfo[kcuu1]}" up-line-or-beginning-search
   bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
   bindkey -M vicmd "${terminfo[kcuu1]}" up-line-or-beginning-search
-fi
+}
 
+# down arrow: fuzzy history search
 autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey -M emacs "^[[B" down-line-or-beginning-search
 bindkey -M viins "^[[B" down-line-or-beginning-search
 bindkey -M vicmd "^[[B" down-line-or-beginning-search
-if [[ -n "${terminfo[kcud1]}" ]]; then
+[[ -n "${terminfo[kcud1]}" ]] && {
   bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
   bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
   bindkey -M vicmd "${terminfo[kcud1]}" down-line-or-beginning-search
-fi
+}
