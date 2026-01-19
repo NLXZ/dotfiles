a9455c47eabd4ea29188b84b3f698e02a028bd89
NLXZ <155401381+NLXZ@users.noreply.github.com>
2026-01-19 13:35:15 +0100

    Update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,5 +2,4 @@
 
 killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
-polybar -q main & done
-bash ~/.config/polybar/dock.sh auto
+polybar -q main & bash ~/.config/polybar/dock.sh auto
